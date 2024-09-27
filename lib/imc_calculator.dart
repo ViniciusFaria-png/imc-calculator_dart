15ae86f120e582add2f0a9f0dcff03d1c05e3255
Vinicius Faria <vinicius.f4ria@gmail.com>
2024-09-27 15:38:31 -0300

    add handling exceptions

--- lib/imc_calculator.dart
+++ lib/imc_calculator.dart
@@ -1,20 +1,43 @@
 
-
-import 'dart:convert';
-import 'dart:io';
-
+import 'package:imc_calculator/exception/nome_invalido_exception.dart';
+import 'package:imc_calculator/models/console_utils.dart';
 import 'package:imc_calculator/models/person.dart';
 
 void execute () {
   print("IMC Calculator!");
-  print("Qual seu nome?");
-  String name = stdin.readLineSync(encoding: utf8) ?? "";
-  print("Digite seu peso em KGs:");
-  String input = stdin.readLineSync(encoding: utf8) ?? "";
-  double weight = double.parse(input);
-  print("Digite sua Altura em metros:");
-  String input2 = stdin.readLineSync(encoding: utf8) ?? "";
-  double height = double.parse(input2);
+  String name = ConsoleUtils.lerStringComTexto("Qual seu nome?");
+  try{
+    if(name.trim() == ""){
+        throw NomeInvalidoException();
+    }
+  } catch(NomeInvalidoException){
+    name= "Name";
+    print(NomeInvalidoException);
+  } catch(e) {
+    print(e);
+  }
+
+  double? weight;
+  double? height;
+  try{
+     weight = ConsoleUtils.lerDoubleComTexto("Qual seu peso em KGs:");
+    if( weight! <= 0){
+      throw ArgumentError("O peso deve ser maior que 0.");
+    }
+  } catch (e) {
+    print("Erro ao ler o peso: $e");
+    return;
+  }
+
+  try{
+     height = ConsoleUtils.lerDoubleComTexto("Qual seu peso em KGs:");
+    if( height! <= 0){
+      throw ArgumentError("A altura deve ser maior que 0.");
+    }
+  } catch (e) {
+    print("Erro ao ler a altura: $e");
+    return;
+  }
 
   var person = Person(name, weight, height);
 
